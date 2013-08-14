4f2a03dada91005f11ee3274847f3c78cf2df33f
Rex <Rex.whitten@gmail.com>
2013-08-13 20:02:58 -0400

    adding redis nad links

--- battlespace.server.js
+++ battlespace.server.js
@@ -5,4 +5,14 @@ battlespace.server = {};
 var express = require('express');
 battlespace.server.app = express();
 
+battlespace.server.app.use(express.static('client'));
 
+battlespace.server.app.get('/test', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
+
+battlespace.server.app.listen(3000);
+console.log('Listening on port 3000');
