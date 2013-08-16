6716d29d543a91e8f3003250da12238b569137af
Rex Whitten <rex.whitten@acm.org>
2013-08-16 15:04:43 -0400

    working GET/SET HTTP GET/POST Example

--- server.js
+++ server.js
@@ -1,40 +1,63 @@
+// Root Object for the Server
+var _space = {};
 
-var battlespace = {};
-
-// libs 
+// Libs (Dependencies) 
 var express = require('express');
 var redis = require('redis');
 
-
 // SERVER 
-battlespace.server = {};
-battlespace.tick = 0;
+_space.server = {};
+_space.tick = 0;
 
 // Data-Models
-battlespace.server.client = redis.createClient(6379);
-battlespace.server.client.on("error",function (err) {
+_space.server.client = redis.createClient(6379);
+_space.server.client.on("error",function (err) {
         console.log("Error " + err);
 }); 
 
 
-// HTTP Server
-battlespace.server.app = express();
+// HTTP Server Setup 
+_space.server.app = express();
+_space.server.app.engine('html', require('ejs').renderFile);
+_space.server.app.use(express.static('client')); // Treat the Client Folder as Static Content
+_space.server.app.use(express.bodyParser());
 
-battlespace.server.app.use(express.static('client'));
-
-battlespace.server.app.get('/test', function (req, res) {
-
-    battlespace.tick++;
-    battlespace.server.client.set("update:", battlespace.tick.toString());
-
-    var body = 'Hello World' + battlespace.tick;
+//Route: Test 
+_space.server.app.get('/test', function (req, res) {
+    var body = 'Hello World' + _space.tick;
     res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Content-Length', body.length);
     res.end(body);
-
 });
 
-battlespace.server.app.listen(3000);
+_space.server.app.get('/form', function (req, res) {
+    res.render('node_form.html');
+});
+
+// REDIS: GET
+_space.server.app.get('/data', function (req, res) {
+    var body = "{}";
+    console.log("getting: " + req.query.key);
+
+    _space.server.client.get(req.query.key, function (err, data) {
+        if (err) { console.log(err); }
+        else 
+        {console.log(data);  res.end(data); redis.print();}
+    });
+});
+
+// REDIS: SET
+_space.server.app.post('/data', function (req, res) {
+    var _data = req.body;
+    console.log('saving ' + req.query.key);
+    _space.server.client.set(req.query.key, _data, function (err) {
+        if (err) {
+            console.log(error);
+        }
+    });
+});
+
+_space.server.app.listen(3000);
 console.log('Listening on port 3000');
 
 
